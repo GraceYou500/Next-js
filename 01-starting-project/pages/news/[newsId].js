4fe16f2d00dd072f75e3025d6c4c4ee33be8ac80
Grace You <graceyouya500@gmail.com>
2022-09-01 15:31:34 +1000

    nested router

--- 01-starting-project/pages/news/[newsId].js
+++ 01-starting-project/pages/news/[newsId].js
@@ -1,6 +1,17 @@
 // our-domain.com/news/important-ths
 
+import { useRouter } from "next/router";
+
 function DetailPage() {
+  const router = useRouter(); // return an object with methods we can use
+
+  console.log(router.query.newsId); // the property holds the value of url dynamic parameter.
+
+  const newsId = router.query.newsId;
+
+  // send a request to the backend API
+  // to fetch news item with newsId
+
   return <h1>The Detail Page</h1>;
 }
 
--- 01-starting-project/pages/news/index.js
+++ 01-starting-project/pages/news/index.js
@@ -1,7 +1,26 @@
 // our-domain.com/news
+import Link from "next/link";
+
+import { Fragment } from "react";
 
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>;
+      <ul>
+        <li>
+          <Link href="/news/next-js-is-a-framework">
+            Next Js Is a Great Framework
+          </Link>
+        </li>
+        <li>
+          <a href="/news/learning-it-need-patients">
+            Learning IT is pation need
+          </a>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
